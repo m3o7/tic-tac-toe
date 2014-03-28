afa4abc05a036df37c7da5d1ed8a4a8dfd5bd096
Marco Pashkov <marco.pashkov@sap.com>
2014-03-28 01:35:55 -0700

    refactored random player

--- players/random_computer_player.js
+++ players/random_computer_player.js
@@ -1,65 +1,25 @@
 // implements the Player-interface for a computer
 // TODO: currently the computer-player is hard-coded to a 3x3 field
-var RandomComputerPlayer = function(name, symbol){
-
-    var me = this;
+var RandomComputerPlayer = BasePlayer.extend({
 
     // -PRIVATE
-    function init(name, symbol){
-        me.name = name;
-        me.symbol = symbol;    
-    };
+    init: function(name, symbol){
+        this._super(name, symbol);
+        this.isComputerPlayer = true;
+    },
 
     // +PUBLIC
-    function getSymbol(){
-        return me.symbol;
-    };
-
-    // +PUBLIC
-    function getName(){
-        return me.name;
-    };
-
-    //  +PUBLIC
-    function isComputer(){
-        // HELPER METHOD
-        return true;
-    }
-
-    // +PUBLIC
-    function makeMove(game, board){
+    makeMove: function(game, board){
         // select a random field
         if (typeof field === 'undefined' || typeof field.getInstVar('value') !== 'undefined') {
             // something went wrong - pick a random empty field
-            field = getRandomField(board);
+            field = this.getRandomField(board);
         }
 
         // commit the pick
-        board.setFieldValue(mePointer, field.getInstVar('y'), field.getInstVar('x'));
+        board.setFieldValue(this, field.getInstVar('y'), field.getInstVar('x'));
 
         // notify the game-controller about the move
         game.playerMoved();
-    };
-
-    // -PRIVATE
-    function getRandomField(board){
-        // Return a random field - DEBUG ONLY
-
-        // find empty fields - implicitly there is always one empty field
-        var emptyFields = board.getFields().filter(function(field){
-            return (typeof field.getInstVar('value') === 'undefined');
-        });
-        return emptyFields[Math.floor(Math.random()*emptyFields.length)];
-    };
-
-    var mePointer = {
-        getSymbol   : getSymbol,
-        getName     : getName,
-        isComputer  : isComputer,
-        makeMove    : makeMove,
-    };
-
-    init(name, symbol);
-    // specify public interface
-    return mePointer;
-}
+    },
+});
